9a720a3631d707c9e26735bc9eee5aada31fc976
Jarus <asuraj20@yahoo.com>
2021-07-03 09:34:43 -0700

    Modified Files

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import MainNavigator from './navigation/MainNavigator';
@@ -12,14 +11,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgreen',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
